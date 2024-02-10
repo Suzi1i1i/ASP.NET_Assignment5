0145ef941e1c601b8013964bcb28d4e96ab2c2c1
suzie <puppychico0930@gmail.com>
2024-02-09 18:32:56 -0700

    Add project files.

---
+++ assignment5/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#calculate').click(function () {
+        var hours = $('#hours').val();
+        var rate = $('#rate').val();
+        if (hours > 0) {
+            var total = hours * rate;
+            $('#total').val(total.toFixed(2));
+        } else {
+            alert('Please enter a positive number for hours.');
+            $('#hours').val('').focus();
+        }
+    });
+});
